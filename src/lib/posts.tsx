a02554ae8b3ac577a7bbe1a517297ae0aef0fea0
hhjeee <yoonhj0315@gmail.com>
2024-12-24 11:22:48 +0900

    [fix] decode category in url to resolve file path issues

--- src/lib/posts.tsx
+++ src/lib/posts.tsx
@@ -1,21 +1,21 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { PostData, PostMeta } from "@/types/post";
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { PostData, PostMeta } from '@/types/post';
 
-const postsDirectory = path.join(process.cwd(), "src/posts");
+const postsDirectory = path.join(process.cwd(), 'src/posts');
 
 export function getAllPosts(): PostData[] {
   const categories = fs.readdirSync(postsDirectory);
 
-  const posts =  categories.flatMap((category) => {
+  const posts = categories.flatMap((category) => {
     const categoryPath = path.join(postsDirectory, category);
     const fileNames = fs.readdirSync(categoryPath);
 
     return fileNames.map((fileName) => {
-      const slug = fileName.replace(/\.mdx$/, ""); // 확장자 제거
+      const slug = fileName.replace(/\.mdx$/, ''); // 확장자 제거
       const filePath = path.join(categoryPath, fileName);
-      const fileContents = fs.readFileSync(filePath, "utf8");
+      const fileContents = fs.readFileSync(filePath, 'utf8');
       const { data } = matter(fileContents);
 
       // `slug` 속성이 중복되지 않도록 명시적으로 지정
@@ -25,19 +25,23 @@ export function getAllPosts(): PostData[] {
         date: data.date,
         desc: data.desc,
         image: data.image,
-        category
+        category,
       } as PostData;
     });
   });
 
   posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-  
+
   return posts;
 }
 
-export function getPostData(slug: string): { meta: PostMeta; content: string } {
-  const filePath = path.join(postsDirectory, `${slug}.mdx`);
-  const fileContents = fs.readFileSync(filePath, "utf8");
+export function getPostData(
+  category: string,
+  slug: string
+): { meta: PostMeta; content: string } {
+  const decodedCategory = decodeURIComponent(category);
+  const filePath = path.join(postsDirectory, decodedCategory, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(filePath, 'utf8');
   const { data, content } = matter(fileContents);
 
   return {
