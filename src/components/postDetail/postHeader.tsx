5f6bde730f373da220d6a5bb048597520982fd43
hhjeee <yoonhj0315@gmail.com>
2025-01-03 11:12:27 +0900

    [feat] move to category page

--- src/components/postDetail/postHeader.tsx
+++ src/components/postDetail/postHeader.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import { PostMeta } from '@/types/post';
 import dayjs from 'dayjs';
 
@@ -8,11 +12,20 @@ const PostHeader = ({
   meta: PostMeta;
   category: string;
 }) => {
+  const router = useRouter();
+
+  const handleCategoryClick = (category: string) => {
+    router.push(`/${decodeURIComponent(category)}`);
+  };
+
   return (
     <>
       <h1>{meta.title}</h1>
       <div className="flex items-center gap-2 font-medium">
-        <button className="bg-primary rounded-full px-3 py-0.5 text-white m-0">
+        <button
+          className="bg-primary rounded-full px-3 py-0.5 text-white m-0"
+          onClick={() => handleCategoryClick(category)}
+        >
           {decodeURIComponent(category)}
         </button>
         <p className="m-0">{dayjs(meta.date).format('YYYY년 MM월 DD일')}</p>
