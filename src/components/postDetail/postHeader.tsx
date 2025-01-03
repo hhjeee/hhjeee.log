75be7d50e8fe83ff794800e9942d90f47e4345ab
hhjeee <yoonhj0315@gmail.com>
2025-01-03 10:29:29 +0900

    [chore] add category button

--- src/components/postDetail/postHeader.tsx
+++ src/components/postDetail/postHeader.tsx
@@ -11,11 +11,13 @@ const PostHeader = ({
   return (
     <>
       <h1>{meta.title}</h1>
-      <button className="text-primary">{decodeURIComponent(category)}</button>
-      <p className="font-medium m-0">
-        {dayjs(meta.date).format('YYYY년 MM월 DD일')}
-      </p>
-      <hr className="my-[1rem]" />
+      <div className="flex items-center gap-2 font-medium">
+        <button className="bg-primary rounded-full px-3 py-0.5 text-white m-0">
+          {decodeURIComponent(category)}
+        </button>
+        <p className="m-0">{dayjs(meta.date).format('YYYY년 MM월 DD일')}</p>
+      </div>
+      <hr className="my-[1rem] h-0.5 bg-gray1" />
     </>
   );
 };
