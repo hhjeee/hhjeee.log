88341e9d31a11afa98ed70b06baa2c25c838e02d
hhjeee <yoonhj0315@gmail.com>
2025-01-03 11:43:15 +0900

    [chore] remove annotation

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -13,19 +13,15 @@ export function getAllPosts(): PostData[] {
     const fileNames = fs.readdirSync(categoryPath);
 
     return fileNames.map((fileName) => {
-      const slug = fileName.replace(/\.mdx$/, ''); // 확장자 제거
+      const slug = fileName.replace(/\.mdx$/, '');
       const filePath = path.join(categoryPath, fileName);
       const fileContents = fs.readFileSync(filePath, 'utf8');
       const { data } = matter(fileContents);
 
-      // `slug` 속성이 중복되지 않도록 명시적으로 지정
       return {
-        slug: slug,
-        title: data.title,
-        date: data.date,
-        desc: data.desc,
-        image: data.image,
+        slug,
         category,
+        ...data,
       } as PostData;
     });
   });
