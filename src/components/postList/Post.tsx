92767b4933d352828e2c40165a682d561caeafde
hhjeee <yoonhj0315@gmail.com>
2025-01-03 11:30:12 +0900

    [chore] add hover animation

--- src/components/postList/Post.tsx
+++ src/components/postList/Post.tsx
@@ -10,7 +10,7 @@ const Post = ({ posts }: { posts: PostData[] }) => {
         <div key={post.slug}>
           <Link
             href={`/posts/${post.category}/${post.slug}`}
-            className="break-inside-avoid mb-[1rem] p-[1rem] border no-underline block rounded-lg shadow-sm flex flex-col gap-[0.5rem] hover:shadow-lg"
+            className="break-inside-avoid mb-[1rem] p-[1rem] border no-underline block rounded-lg shadow-sm flex flex-col gap-[0.5rem] hover:shadow-lg transition-shadow duration-300 ease-linear"
           >
             {post.image && (
               <div className="relative w-full min-h-[10rem] max-h-[25rem] mb-[0.5rem] rounded-md overflow-hidden">
