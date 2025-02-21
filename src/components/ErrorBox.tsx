23bc73c850ed2179f8bb8b2a5a05d0fa9f57016b
hhjeee <yoonhj0315@gmail.com>
2025-02-21 11:05:59 +0900

    [chore] parsing, toc post

--- src/components/ErrorBox.tsx
+++ src/components/ErrorBox.tsx
@@ -1,16 +1,10 @@
 import { ReactNode } from 'react';
 
-import { TriangleAlertIcon } from 'lucide-react';
-
 const ErrorBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex gap-2 bg-[#FAFAFA] pr-2 rounded-md">
+    <div className="flex gap-2 bg-[#FF3333] bg-opacity-10 pr-2 rounded-md">
       <div className="w-1.5 mr-2 rounded-l-md bg-[#FF3333] h-auto"></div>
       <div className="font-semibold">{children}</div>
-      <TriangleAlertIcon
-        size={28}
-        className="mt-1 text-[#FF3333] stroke-[2.5px]"
-      />
     </div>
   );
 };
--- src/components/HighlightBox.tsx
+++ src/components/HighlightBox.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from 'react';
 
-import { StarIcon } from 'lucide-react';
-
 const HighlightBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex gap-2 bg-[#FAFAFA] pr-2 rounded-md">
+    <div className="flex gap-2 bg-[#FFD94D] bg-opacity-5 pr-2 rounded-md">
       <div className="w-1.5 mr-2 rounded-l-md bg-[#FFD94D] h-auto"></div>
       <div className="font-semibold">{children}</div>
-      <StarIcon size={28} className="mt-1 text-[#FFD94D] stroke-[2.5px]" />
     </div>
   );
 };
