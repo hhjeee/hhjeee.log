012f965a2fcb2aca24e3c9c2aa86362be234ba7a
hhjeee <yoonhj0315@gmail.com>
2025-03-11 18:05:15 +0900

    [chore] getCategory unification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { PostData } from '@/types/post';
 
-import { getAllPosts } from '@/lib/posts';
+import { getAllPosts, getCategories } from '@/lib/posts';
 
 import PostSection from '@/components/postList/PostSection';
 
 const MainPage = async () => {
   const posts: PostData[] = await getAllPosts();
-  const categories = [...new Set(posts.map((post) => post.category))];
+  const categories = await getCategories();
 
   return <PostSection posts={posts} categories={categories} />;
 };
