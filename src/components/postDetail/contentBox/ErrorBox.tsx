4abdf404f46ff7c8bf77575e9aec6e3135bea57e
hhjeee <yoonhj0315@gmail.com>
2025-04-08 12:38:34 +0900

    [chore] content box design

--- src/components/postDetail/contentBox/ErrorBox.tsx
+++ src/components/postDetail/contentBox/ErrorBox.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from 'react';
 
 const ErrorBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex gap-2 bg-[#FF3333] bg-opacity-10 pr-2 rounded-md">
-      <div className="w-1.5 mr-2 rounded-l-md bg-[#FF3333] h-auto"></div>
-      <div className="font-semibold">{children}</div>
+    <div className="flex gap-4 bg-[#FF3333] bg-opacity-10 pr-2 rounded-l-md">
+      <div className="w-1.5 rounded-l-md bg-[#FF3333] h-auto"></div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
--- src/components/postDetail/contentBox/HighlightBox.tsx
+++ src/components/postDetail/contentBox/HighlightBox.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from 'react';
 
 const HighlightBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex gap-2 bg-[#FFD94D] bg-opacity-5 pr-2 rounded-md">
-      <div className="w-1.5 mr-2 rounded-l-md bg-[#FFD94D] h-auto"></div>
-      <div className="font-semibold">{children}</div>
+    <div className="flex gap-4 bg-[#FFD94D] bg-opacity-5 pr-2 rounded-l-md">
+      <div className="w-1.5 rounded-l-md bg-[#FFD94D] h-auto"></div>
+      <div className="flex-1">{children}</div>
     </div>
   );
 };
