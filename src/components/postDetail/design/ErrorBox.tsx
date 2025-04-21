49ec28656dfc4ac11192be26ce0309a02376bd3f
hhjeee <yoonhj0315@gmail.com>
2025-04-21 22:06:59 +0900

    [post] mdx generator post

--- src/components/postDetail/design/ErrorBox.tsx
+++ src/components/postDetail/design/ErrorBox.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from 'react';
 
 const ErrorBox = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex gap-4 bg-[#FF3333] bg-opacity-10 pr-2 rounded-l-md">
-      <div className="w-1.5 rounded-l-md bg-[#FF3333] h-auto"></div>
+    <div className="flex gap-4 bg-[#FF3333] bg-opacity-5 pr-2 rounded-l-md">
+      <div className="w-1.5 rounded-l-md bg-[#FF3333] bg-opacity-90 h-auto"></div>
       <div className="flex-1">{children}</div>
     </div>
   );
