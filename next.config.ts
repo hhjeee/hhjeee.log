4b0a936b4f0d1fac937f3d5cfb749793fab3aba0
hhjeee <yoonhj0315@gmail.com>
2025-05-20 13:14:00 +0900

    [chore] rename folder to lowercase

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,8 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/blog/error1',
-        destination: '/Blog/error1',
-        permanent: true,
-      },
-      {
-        source: '/%EB%B8%94%EB%A1%9C%EA%B7%B8%EA%B0%9C%EB%B0%9C/gfm',
-        destination: '/Blog/gfm',
+        source: '/%EB%B8%94%EB%A1%9C%EA%B7%B8%EA%B0%9C%EB%B0%9C/:slug',
+        destination: '/blog/:slug',
         permanent: true,
       },
     ];
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const NotFound = () => {
   return (
-    <div className="h-full place-content-center text-center">
+    <div className="min-h-[calc(100vh-4.5rem)] place-content-center text-center">
       <h1 className="text-2xl mb-2 font-bold">Not Found</h1>
       <p>페이지를 찾을 수 없습니다.</p>
       <button className="mt-4 w-fit px-6 font-medium bg-primary rounded text-white py-2">
