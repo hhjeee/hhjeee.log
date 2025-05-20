81483e456ab99e0c1a618e15030f5056276f6dc7
hhjeee <yoonhj0315@gmail.com>
2025-05-20 12:45:02 +0900

    [chore] 상위 검색 유입 URL 리다이렉트 처리

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['next-mdx-remote'],
+
+  async redirects() {
+    return [
+      {
+        source: '/blog/error1',
+        destination: '/Blog/error1',
+        permanent: true,
+      },
+      {
+        source: '/%EB%B8%94%EB%A1%9C%EA%B7%B8%EA%B0%9C%EB%B0%9C/gfm',
+        destination: '/Blog/gfm',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
